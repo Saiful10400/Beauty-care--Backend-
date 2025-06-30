41fbf693b07a481b789d2014aa8e2b9595043e83
saiful10400 <saifulislam10400i5@gmail.com>
2025-06-30 23:45:04 +0600

    product updating api updated.

--- src/Modules/Product/product.service.ts
+++ src/Modules/Product/product.service.ts
@@ -1,6 +1,7 @@
-import { FilterQuery } from "mongoose";
+import { FilterQuery, SortOrder } from "mongoose";
 import { productModel } from "./product.model";
 import TProduct from "./product.types";
+import comboOfferModle from "../Offer.Combo/offer.model";
 
 // create product service
 const createProduct = async (payload: TProduct) => {
@@ -16,20 +17,85 @@ const getProductById = async (id: string) => {
     .populate("categoryIds"); // populate category details
   return result;
 };
-// get product by slug service
+// get all products
 const getProducts = async ({
   limit,
   offset,
+  sort,
   ...query
 }: FilterQuery<TProduct>) => {
-  
+  const conditions: FilterQuery<TProduct>[] = [];
+  const order = Number(sort) || 1;
+  if (query.maxPrice || query.minPrice) {
+    const priceCnd = Object.keys(query).map((item) => {
+      if (query.comboOffer) {
+        if (item === "maxPrice") return { price: { $lte: query.maxPrice } };
+        if (item === "minPrice") return { price: { $gte: query.minPrice } };
+      } else {
+        if (item === "maxPrice")
+          return { price: { $lte: query.maxPrice }, haveOffer: false };
+        if (item === "minPrice")
+          return { price: { $gte: query.minPrice }, haveOffer: false };
+      }
+      return {};
+    });
+    conditions.push({ $and: priceCnd });
+  }
+
+  if (query.categoryIds) {
+    conditions.push({ categoryIds: { $in: JSON.parse(query.categoryIds) } });
+  }
+
+  if (query.brandIds) {
+    conditions.push({ brandId: { $in: JSON.parse(query.brandIds) } });
+  }
+
+  if (query.searchTerm) {
+    const regex = new RegExp(query.searchTerm, "i");
+
+    conditions.push({
+      $or: [
+        { name: regex },
+        { description: regex },
+        { shortDescription: regex },
+        { tags: { $in: [regex] } },
+      ],
+    });
+  }
+
+  if (query.inStock) {
+    conditions.push({ inStock: query.inStock === "true" });
+  }
+  if (query.haveOffers) {
+    conditions.push({ haveOffer: query.haveOffers === "true" });
+  }
+
+  console.dir(conditions, { depth: "infinity" });
+
+  if (query.comboOffer) {
+    const result = await comboOfferModle
+      .find({ $and: [...conditions] })
+      .populate("categoryIds")
+      .limit(limit)
+      .skip(offset)
+      .sort({ _id: order as SortOrder });
+    return {
+      result,
+      total: await comboOfferModle.countDocuments({ $and: [...conditions] }),
+    };
+  }
+
   const result = await productModel
-    .find(query)
+    .find({ $and: [...conditions] })
     .populate("brandId")
     .populate("categoryIds")
     .limit(limit)
-    .skip(offset);
-  return { result, total: await productModel.countDocuments(query) };
+    .skip(offset)
+    .sort({ _id: order as SortOrder });
+  return {
+    result,
+    total: await productModel.countDocuments({ $and: [...conditions] }),
+  };
 };
 
 // update product service
